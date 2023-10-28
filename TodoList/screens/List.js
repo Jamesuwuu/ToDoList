a098e8224d74ce6b79e3d2caffb9f333c04dc1a9
Jamesuwuu <z5313388@ad.unsw.edu.au>
2023-10-28 16:06:29 +1100

    added sort

--- TodoList/screens/List.js
+++ TodoList/screens/List.js
@@ -6,12 +6,15 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Details from './Details';
 import { NavigationContainer } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { DateTimePickerAndroid } from '@react-native-community/datetimepicker';
 
 const NOTES_STORE_KEY = '@notes';
 
 export default function List({navigation, route}) {
   const {title, desc, date, image, listNum, del} = route.params ?? {};
   const [list, setList] = React.useState([]);
+  const [sortList, setSortList] = React.useState([]);
+  const [sorted, setSorted] = React.useState(false);
 
   useEffect(() => {if(title || desc || date || image) {
     if (del) {
@@ -45,6 +48,7 @@ export default function List({navigation, route}) {
       await AsyncStorage.setItem(NOTES_STORE_KEY, JSON.stringify(list))
     };
     saveList();
+    updateList();
   }, [list])
 
 
@@ -60,15 +64,60 @@ export default function List({navigation, route}) {
     setList(newList);
   }
 
+  function sortDate() {
+    if (!sorted) {
+      const newList = list.map((old) => {
+        return old;
+      });
+      newList.sort(function (a,b) {
+        if (a.title < b.title) {
+          return -1;
+        }
+        if (a.title > b.title) {
+          return 1;
+        }
+        return 0;
+      });
+      setSortList(newList);
+    }
+    setSorted(!sorted);
+  }
+
+  function updateList() {
+    const newList = list.map((old) => {
+      return old;
+    });
+    newList.sort(function (a,b) {
+      if (a.title < b.title) {
+        return -1;
+      }
+      if (a.title > b.title) {
+        return 1;
+      }
+      return 0;
+    });
+    setSortList(newList);
+  }
+
   return (
     <View style={styles.container}>
       {
+        sorted ?
+        sortList.map(({title, desc, date, image}, index) => (
+          <Todo title={title} desc={desc} date={date} image={image} navigation={navigation} key={index} listNum={index}/>
+        ))
+        :
         list.map(({title, desc, date, image}, index) => (
           <Todo title={title} desc={desc} date={date} image={image} navigation={navigation} key={index} listNum={index}/>
         ))
       }
       <Pressable style={styles.button} onPress={() => navigation.navigate("Add Todo")}>
         <Text>Add Todo</Text>
+      </Pressable>
+      <Pressable style={styles.sortButton} onPress={() => sortDate()}>
+        {
+          sorted ?  <Text>Unsort</Text> :  <Text>Sort</Text>
+        }
       </Pressable>
     </View>
   );
@@ -88,5 +137,13 @@ const styles = StyleSheet.create({
     position: 'absolute',
     bottom: 20,
     left: 20,
+  },
+  sortButton: {
+    padding: 10,
+    borderRadius: 50,
+    backgroundColor: 'rgb(210, 230, 255)',
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
   }
 });
